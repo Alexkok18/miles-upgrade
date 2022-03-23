78ecd7b0491aca7931168542407bd8454cdc916e
Алексей Полетаев <Alexkok18@mail.ru>
2022-03-23 22:58:23 +0300

    upgrade commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // входные данные
-        int ticket = 1000;
-
-        // логика программы
-        int miles = ticket / 20;
-        System.out.println("Вам начислено");
-        System.out.println(miles);
-        System.out.println("миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Вам начислено " + miles + " миль");
     }
 }
